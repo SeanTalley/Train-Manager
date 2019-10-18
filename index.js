9b08e4daff26ceafc28debfc55b42d306c2484d2
SeanTalley <55466559+SeanTalley@users.noreply.github.com>
2019-10-18 10:42:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express')
+	, http = require('http');
+//var cookieParser = require('cookie-parser');
+//make sure you keep this order
+var app = express();
+//app.use(cookieParser());
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/www'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/www/index.html");
+});
+var authentication = [
+  "KHCKV3KJ234KLH435KH24356KH2KHN3H23K54KK", //ID = 0
+  "NBN234NB435UY4325N2CB45UCUYUYBN34NV23BN"  //ID = 1
+];
+var msgProps = ["id","lat","lng","text","color","carType","speed","status","statusColor","origin","destination","eta","img"];
+io.on('connection', function (socket) {
+  socket.on("update", function(msg) {
+    if(!msg.hasOwnProperty("key")) {
+      socket.emit("response","Missing Authorization Key");
+      return;
+    }
+    if(msg.key !== authentication[msg.id]) {
+      socket.emit("response","Invalid Authorization Key");
+      return;
+    }
+    if(!msgProps.every(function(x) { return x in msg })) {
+      socket.emit("response", "Missing Message Data");
+      return;
+    }
+    delete msg.key; //Don't pass along the key - throw it away
+    socket.broadcast.emit("update", msg);
+    socket.emit("response","Success");
+  });
+});
+server.listen(8080, function () {
+	console.log('listening on *:8080');
+});
