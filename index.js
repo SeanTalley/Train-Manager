847edd11a21315ab858c311f63b07f4173250216
SeanTalley <55466559+SeanTalley@users.noreply.github.com>
2019-10-18 10:43:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require('express')
-	, http = require('http');
+var express = require('express'),
+    http = require('http');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
